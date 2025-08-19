84185f2688c58fe54be8f4cb535548a2b4dee141
Jake Morrissey <jakemorrissey@utexas.edu>
2025-08-19 16:15:22 -0700

    outline

--- gosplitmail.go
+++ gosplitmail.go
@@ -1,5 +1,19 @@
 package gosplitmail
 
-func SplitEmail() {
+import (
+	"gopkg.in/gomail.v2"
+)
+
+func SplitEmail(body, subject, from string, groups []string) []*gomail.Message {
+	messages := make([]*gomail.Message, len(groups))
+	for i := range len(groups) {
+		message := gomail.NewMessage()
+		message.SetHeader("From", from)
+		message.SetHeader("To", groups[i])
+		message.SetHeader("Subject", subject)
+		message.SetBody("text/html", body)
+		messages[i] = message
+	}
+	return messages
 
 }
