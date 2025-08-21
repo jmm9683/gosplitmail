90df8083b2a5bf98f85d829f8185211674f5d615
Jake Morrissey <jakemorrissey@utexas.edu>
2025-08-20 23:13:54 -0700

    expanding on the idea

--- cmd/main.go
+++ cmd/main.go
@@ -2,69 +2,67 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/rwcarlsen/goexif/exif"
-	"github.com/rwcarlsen/goexif/mknote"
-	"gopkg.in/gomail.v2"
+	"github.com/jakemorrissey/go-splitmail/gosplitmail"
 )
 
 func main() {
+	// Email template using all fields
+	bodyTemplate := `
+    <h2>{{.ThreadTitle}}</h2>
+    <p>Recipients: {{.ThreadList}}</p>
+    {{if .ThreadFilter.Family}}<p>Hi fam!!</p>{{end}}
+    {{if .ThreadFilter.Friends}}<p>Hey frens!</p>{{end}}
+    {{if .ThreadFilter.Colleague}}<p>Dear Colleagues, </p>{{end}}
+    {{range $i, $cid := .ImageCIDs}}
+        <img src="cid:{{$cid}}" alt="Photo {{$i}}">
+    {{end}}
+    `
 
-	photoPath := filepath.Join("./Photos")
-	jpgPattern := filepath.Join(photoPath, "*.jpg")
-
-	photos, err := filepath.Glob(jpgPattern)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
+	// Example EmailData for each group
+	groups := []gosplitmail.EmailData{
+		{
+			ThreadTitle: "To the family",
+			ThreadList:  "dad@example.com",
+			ImagePaths:  []string{"./Photos/dad1.jpg", "./Photos/dad2.jpg"},
+			ImageCIDs:   []string{"dadphoto1", "dadphoto2"},
+			ThreadFilter: map[string]any{
+				"Dad": true,
+			},
+		},
+		{
+			ThreadTitle: "To the friends",
+			ThreadList:  "friend@example.com",
+			ImagePaths:  []string{"./Photos/friend1.jpg"},
+			ImageCIDs:   []string{"friendphoto1"},
+			ThreadFilter: map[string]any{
+				"Friend": true,
+			},
+		},
+		{
+			ThreadTitle: "To the colleagues",
+			ThreadList:  "colleague@example.com",
+			ImagePaths:  []string{"./Photos/work1.jpg", "./Photos/work2.jpg"},
+			ImageCIDs:   []string{"workphoto1", "workphoto2"},
+			ThreadFilter: map[string]any{
+				"Colleague": true,
+			},
+		},
 	}
 
-	uniqueImageMap := map[int64]bool{} //tbd: sqlite
+	subject := "Weekly Recap"
+	from := "me@example.com"
 
-	for _, photo := range photos {
-		imageDescription := ""
-		f, err := os.Open(photo)
-		if err != nil {
-			log.Fatal(err)
-		}
+	messages := gosplitmail.SplitEmail(bodyTemplate, subject, from, groups)
 
-		exif.RegisterParsers(mknote.All...)
-
-		x, err := exif.Decode(f)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		tm, _ := x.DateTime()
-		if uniqueImageMap[tm.Unix()] {
-			fmt.Println("Duplicate image: ", photo, tm.Unix())
-			continue
-		} else {
-			uniqueImageMap[tm.Unix()] = true //tbd: sqlite
-		}
-		year, month, day := tm.Date()
-		fmt.Printf("Taken on %v %v, %v\n", month, day, year)
-		imageDescriptionTiff, _ := x.Get(exif.ImageDescription)
-		imageDescription = strings.ReplaceAll(imageDescriptionTiff.String(), "\"", "")
-		fmt.Println(imageDescription)
+	// Example: Print the email bodies to stdout
+	for i, msg := range messages {
+		fmt.Printf("Email #%d to %s:\n%s\n\n", i+1, groups[i].ThreadList, msg.GetBody("text/html"))
 	}
 
-	m := gomail.NewMessage()
-	m.SetHeader("From", "SENDER@SENDER.COM")
-	m.SetHeader("To", "RECIPIENT@RECIPIENT.COM")
-	m.SetHeader("Subject", "Hello!")
-	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
-	// m.Attach("/home/Alex/lolcat.jpg")
-
-	// d := gomail.NewDialer("smtp.mail.me.com", 587, "MEICLOUD@ME.COM", "TOlKENTIME")
-
-	// // Send the email to Bob, Cora and Dan.
-	// if err := d.DialAndSend(m); err != nil {
-	// 	panic(err)
-	// }
-
+	// To actually send, use gomail.Dialer (not shown here)
+	// d := gomail.NewDialer("smtp.example.com", 587, "user", "pass")
+	// if err := d.DialAndSend(messages...); err != nil {
+	//     fmt.Println("Send error:", err)
+	//     os.Exit(1)
 }
--- gosplitmail.go
+++ gosplitmail.go
@@ -1,19 +1,45 @@
 package gosplitmail
 
 import (
+	"bytes"
+	"html/template"
+
 	"gopkg.in/gomail.v2"
 )
 
-func SplitEmail(body, subject, from string, groups []string) []*gomail.Message {
+type EmailData struct {
+	ThreadTitle  string
+	ThreadList   string
+	ImagePaths   []string // paths to images
+	ImageCIDs    []string // CIDs for inline embedding
+	ThreadFilter map[string]any
+}
+
+func SplitEmail(bodyTemplate, subject, from string, groups []EmailData) []*gomail.Message {
+	tmpl, err := template.New("email").Parse(bodyTemplate)
+	if err != nil {
+		panic(err)
+	}
+
 	messages := make([]*gomail.Message, len(groups))
-	for i := range len(groups) {
+	for i, data := range groups {
+		var buf bytes.Buffer
+		if err := tmpl.Execute(&buf, data); err != nil {
+			panic(err)
+		}
 		message := gomail.NewMessage()
 		message.SetHeader("From", from)
-		message.SetHeader("To", groups[i])
+		message.SetHeader("To", data.ThreadList)
 		message.SetHeader("Subject", subject)
-		message.SetBody("text/html", body)
+		message.SetBody("text/html", buf.String())
+		for j := range data.ImagePaths {
+			if data.ImagePaths[j] != "" && data.ImageCIDs[j] != "" {
+				message.Embed(data.ImagePaths[j], gomail.SetHeader(map[string][]string{
+					"Content-ID": {"<" + data.ImageCIDs[j] + ">"},
+				}))
+			}
+		}
 		messages[i] = message
 	}
 	return messages
-
 }
